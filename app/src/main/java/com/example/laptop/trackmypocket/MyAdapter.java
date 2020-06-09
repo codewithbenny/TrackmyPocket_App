42d22d3bb70e51eaba962dc33969a8a884eafde3
codewithbenny <codewithbenny999@gmail.com>
2020-06-09 14:59:06 +0530

    Track-my-pocket

---
+++ app/src/main/java/com/example/laptop/trackmypocket/MyAdapter.java
@@ -0,0 +1,45 @@
+package com.example.laptop.trackmypocket;
+
+/**
+ * Created by laptop on 05-07-2018.
+ */
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends ArrayAdapter {
+
+    ArrayList birdList = new ArrayList<>();
+
+    public MyAdapter(Context context, int textViewResourceId, ArrayList objects) {
+        super(context, textViewResourceId, objects);
+        birdList = objects;
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        v = inflater.inflate(R.layout.grid_view_items, null);
+        TextView textView = (TextView) v.findViewById(R.id.textView);
+        ImageView imageView = (ImageView) v.findViewById(R.id.imageView);
+        Item i1=(Item) birdList.get(position);
+       // Item i2=(Item) birdList.get(position);
+        textView.setText(i1.getbirdName());
+        imageView.setImageResource(i1.getbirdImage());
+        return v;
+
+    }
+
+}
